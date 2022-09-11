3fd3754a1d0594afe1efd4138cdcd8d07059cc5d
Connor Engstrom <connor.e.music@gmail.com>
2022-09-11 14:21:01 -0400

    Retrieve files from database dynamically, and correctly sends to browser for download

--- Controllers/TabsController.cs
+++ Controllers/TabsController.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
-using System;
+using System.Net.Http.Headers;
 using System.Configuration;
 using Microsoft.Data.SqlClient;
 using System.Data;
 using _30DaysOfShred.Models;
+using Microsoft.Data.SqlTypes;
+using System.Text;
+using Microsoft.AspNetCore.StaticFiles;
 
 namespace _30DaysOfShred.Controllers
 {
@@ -20,6 +23,100 @@ namespace _30DaysOfShred.Controllers
         public IActionResult Search()
         {
             return View();
+        }
+
+        [Authorize]
+        public string DownloadLink()
+        {
+            SqlConnection con = new SqlConnection("Data Source=DEEPTHOUGHT-2\\SQLEXPRESS;Initial Catalog=guitar_tabs_database;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand("SELECT file_stream.GetFileNamespacePath(1, 2) FROM guitar_tabs_files WHERE stream_id = @idParam", con);
+            SqlParameter idParam = new SqlParameter("@idParam", SqlDbType.UniqueIdentifier);
+            idParam.Value = new Guid("8E91CDB4-602E-ED11-8D84-04ED33527636");
+            cmd.Parameters.Add(idParam);
+
+            string? filePath = null;
+            Object pathObject = cmd.ExecuteScalar();
+            if (DBNull.Value != pathObject)
+            {
+                filePath = (string)pathObject;
+            }
+            else
+            {
+                throw new System.Exception("problem");
+            }
+
+            return filePath;
+        }
+
+        [Authorize]
+        [HttpGet("download/{streamId}")]
+        //[Route("downloadfile/{streamId}")]
+        public IActionResult DownloadFile(string streamId)
+        {
+            Console.WriteLine(streamId);
+            var data = GuitarTab.GetGuitarTabData(streamId);
+            var filename = GuitarTab.GetGuitarTabFileName(streamId);
+            var result = new HttpResponseMessage(System.Net.HttpStatusCode.OK);
+            var stream = new MemoryStream(data);
+            result.Content = new StreamContent(stream);
+            result.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment");
+            result.Content.Headers.ContentDisposition.FileName = filename;
+            result.Content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+            return File(data, "application/octet-stream", filename);
+        }
+
+        [Authorize]
+        public void Download(/*string guitarTabId*/)
+        {
+            SqlConnection con = new SqlConnection("Data Source=DEEPTHOUGHT-2\\SQLEXPRESS;Initial Catalog=guitar_tabs_database;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand("SELECT file_stream.GetFileNamespacePath(1, 2) FROM guitar_tabs_files WHERE stream_id = @idParam", con);
+            SqlParameter idParam = new SqlParameter("@idParam", SqlDbType.UniqueIdentifier);
+            idParam.Value = new Guid("8E91CDB4-602E-ED11-8D84-04ED33527636");
+            cmd.Parameters.Add(idParam);
+
+            string? filePath = null;
+            string? fileName = null;
+            Object pathObject = cmd.ExecuteScalar();
+            if (DBNull.Value != pathObject)
+            {
+                filePath = (string)pathObject;
+            }
+            else
+            {
+                throw new System.Exception("problem");
+            }
+
+            SqlCommand cmd2 = new SqlCommand("SELECT name FROM guitar_tabs_files WHERE stream_id = @idParam2", con);
+            SqlParameter idParam2 = new SqlParameter("@idParam2", SqlDbType.UniqueIdentifier);
+            idParam2.Value = new Guid("8E91CDB4-602E-ED11-8D84-04ED33527636");
+            cmd2.Parameters.Add(idParam2);
+
+            SqlDataReader sqlDataReader = cmd2.ExecuteReader();
+            while (sqlDataReader.Read()) fileName = sqlDataReader["name"].ToString();
+
+            //HttpClient client = new HttpClient();
+            //try
+            //{
+            //    HttpResponseMessage response = await client.GetAsync(filePath);
+            //    response.EnsureSuccessStatusCode();
+            //    string responseBody = await response.Content.ReadAsStringAsync();
+            //    Console.WriteLine(responseBody);
+            //}
+            //catch (HttpRequestException e)
+            //{
+            //    Console.WriteLine("HttpRequestException caught");
+            //    Console.WriteLine(e.Message);
+            //}
+
+            using (var client = new System.Net.WebClient())
+            {
+                client.DownloadFile(filePath, fileName);
+                Console.WriteLine("file downloading");
+            }
         }
 
         public ActionResult ShowSearchResults(string searchPhrase)
--- Models/GuitarTab.cs
+++ Models/GuitarTab.cs
@@ -1,4 +1,7 @@
-﻿namespace _30DaysOfShred.Models
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace _30DaysOfShred.Models
 {
     public class GuitarTab
     {
@@ -19,6 +22,68 @@
             this.uniqueId = Guid.Empty;
             this.Title = "";
             this.TabCategories = new List<string>();
+        }
+
+        //public static string GetDownloadLink()
+        //{
+        //    SqlConnection con = new SqlConnection("Data Source=DEEPTHOUGHT-2\\SQLEXPRESS;Initial Catalog=guitar_tabs_database;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        //    con.Open();
+
+        //    SqlCommand cmd = new SqlCommand("SELECT file_stream.GetFileNamespacePath(1, 2) FROM guitar_tabs_files WHERE stream_id = @idParam", con);
+        //    SqlParameter idParam = new SqlParameter("@idParam", SqlDbType.UniqueIdentifier);
+        //    idParam.Value = new Guid("8E91CDB4-602E-ED11-8D84-04ED33527636");
+        //    cmd.Parameters.Add(idParam);
+
+        //    string? filePath = null;
+        //    Object pathObject = cmd.ExecuteScalar();
+        //    if (DBNull.Value != pathObject)
+        //    {
+        //        filePath = (string)pathObject;
+        //    }
+        //    else
+        //    {
+        //        throw new System.Exception("problem");
+        //    }
+
+        //    return filePath;
+        //}
+
+        public static byte[] GetGuitarTabData(string stream_id)
+        {
+            SqlConnection con = new SqlConnection("Data Source=DEEPTHOUGHT-2\\SQLEXPRESS;Initial Catalog=guitar_tabs_database;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            con.Open();
+            byte[] GuitarTabData;
+
+            SqlCommand cmd = new SqlCommand("SELECT file_stream FROM guitar_tabs_files WHERE stream_id = @idParam", con);
+            SqlParameter idParam = new SqlParameter("@idParam", SqlDbType.UniqueIdentifier);
+            idParam.Value = new Guid(stream_id);
+            cmd.Parameters.Add(idParam);
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                reader.Read();
+                GuitarTabData = (byte[])reader["file_stream"];
+            }
+            con.Close();
+            return GuitarTabData;
+        }
+
+        public static string GetGuitarTabFileName(string stream_id)
+        {
+            SqlConnection con = new SqlConnection("Data Source=DEEPTHOUGHT-2\\SQLEXPRESS;Initial Catalog=guitar_tabs_database;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            con.Open();
+            string GuitarTabFileName = "download";
+
+            SqlCommand cmd = new SqlCommand("SELECT name FROM guitar_tabs_files WHERE stream_id = @idParam", con);
+            SqlParameter idParam = new SqlParameter("@idParam", SqlDbType.UniqueIdentifier);
+            idParam.Value = new Guid(stream_id);
+            cmd.Parameters.Add(idParam);
+
+            SqlDataReader sqlDataReader = cmd.ExecuteReader();
+            while (sqlDataReader.Read()) GuitarTabFileName = sqlDataReader["name"].ToString();
+
+            con.Close();
+            if (GuitarTabFileName == null) return "download";
+            return GuitarTabFileName;
         }
     }
 }
